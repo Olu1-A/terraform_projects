9a0e36dd94d86e58ff554bc69523fb130f16222f
geebeenext <115200176+geebeenext@users.noreply.github.com>
2023-03-23 11:05:33 -0600

    Update provider.tf

--- s3/provider.tf
+++ s3/provider.tf
@@ -52,6 +52,82 @@ provider "aws" {
   
   
   
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   /*provider "aws" {
   region = var.aws_region
 }
@@ -75,3 +151,43 @@ output "s3_bucket_name" {
 output "s3_bucket_region" {
     value = aws_s3_bucket.my-s3-bucket.region
 }*/
+/*
+variable "random" {
+  description = "needs theoutput from random integer"
+  type = string
+  default = "random_integer.random_figure.result"
+}
+*/
+
+/*variable "aws_region" {
+  description = "The AWS region to use to create resources."
+  default     = "us-east-2"
+}
+
+variable "bucket_prefix" {
+    type        = string
+    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix. Conflicts with bucket."
+    default     = "my-s3bucket-"
+}
+
+variable "tags" {
+    type        = map
+    description = "(Optional) A mapping of tags to assign to the bucket."
+    default     = {
+        environment = "DEV"
+        terraform   = "true"
+    }
+}
+
+variable "versioning" {
+    type        = bool
+    description = "(Optional) A state of versioning."
+    default     = true
+}
+
+variable "acl" {
+    type        = string
+    description = " Defaults to private "
+    default     = "private"
+}
+*/
