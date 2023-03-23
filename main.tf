e9c68abba2f03ca1a60147c36dcce513d7eb0d82
geebeenext <115200176+geebeenext@users.noreply.github.com>
2023-03-23 10:59:18 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,17 +10,3 @@ output "module_random" {
 output "aws_kms_key" {
   value = module.s3.aws_kms_key
 }
-/*provider "aws" {
-  region = var.aws_region
-}
-
-resource "aws_s3_bucket" "my-s3-bucket" {
-  bucket_prefix = var.bucket_prefix
-  acl = var.acl
-  
-   versioning {
-    enabled = var.versioning
-  }
-  
-  tags = var.tags
-}*/
